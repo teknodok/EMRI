c79b86beb7af21d58a8e7e0c999b58466b9d7772
teknodok <teknodok@gmail.com>
2025-09-18 17:34:34 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -7,12 +7,12 @@ class Config:
     DB_SERVER = os.environ.get('DB_SERVER', 'localhost')
     DB_DATABASE = 'Q_Ops'
     DB_USERNAME = os.environ.get('DB_USERNAME', 'sa')
-    DB_PASSWORD = os.environ.get('DB_PASSWORD', '_eJE86KYD:c#g9cg')
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', '_9q29838922cg')
     DB_TRUSTED_CONNECTION = os.environ.get('DB_TRUSTED_CONNECTION', 'yes')
     # Email / OTP
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'vishal_jayaswal@emri.in')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', 'hqij vcvp vmvf ksln')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', 'vishal@ri.in')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', ' ksln')
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', 'vishal_jayaswal@emri.in')
